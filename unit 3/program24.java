11d4c3e3c5e81c5525b28f8af9d47beb65c1a316
nilaxichovatiya137091-coder <nilaxi.chovatiya137091@marwadiuniversity.ac.in>
2026-09-11 12:00:31 +0530

    Fix formatting and ensure proper class syntax

--- unit 3/program24.java
+++ unit 3/program24.java
@@ -2,7 +2,7 @@ class program24 {
     public static void main(String args[]) {
         try {
             int a = 10, b = 0;
-            int c = a / b;   // Exception occurs
+            int c = a / b;  
             System.out.println(c);
         }
         catch (ArithmeticException e) {
